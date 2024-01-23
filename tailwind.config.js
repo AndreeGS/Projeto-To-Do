897c09d89a4a816d83be99f2cb0e1f7d9c8d95bd
Tayane Milagres <devtaay@gmail.com>
2024-01-23 20:16:05 -0300

    Começo do desenvolvimento da Home

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'marinho': '#022541',
+      'marinho-suave': '##183851',
+      'ciano': '#02C986',
+      'cinza': '#C7C9C8',
+      'fundo': '#F8F8F8',
+    },
+  },
+  plugins: [],
+}
+
