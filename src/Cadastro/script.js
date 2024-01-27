202b9d28fe21d8ae880d782d0d4ef92bbf2f593d
Tayane Milagres <devtaay@gmail.com>
2024-01-26 21:16:38 -0300

    requisição cadastro sem teste

---
+++ src/Cadastro/script.js
@@ -0,0 +1,47 @@
+var btnCadastrar = document.getElementById("btnCadastrar");
+
+btnCadastrar.addEventListener("click", Cadastro);
+
+async function Cadastro() {
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+
+    // Verificar campos vazios
+    if (!nome || !email || !senha) {
+        alert('Por favor, preencha todos os campos.');
+        return;
+    }
+
+    // Objeto com os dados do cadastro
+    var dadosCad = {
+        id: 0,
+        nome: String(nome),
+        email: String(email),
+        password: String(senha)
+    };
+
+    // Faça uma requisição à API usando o método fetch
+    try {
+        const resposta = await fetch('https://sua-api.com/api/usuario/cadastrar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dadosCad)
+        });
+
+        if (resposta.ok) {
+            try {
+                const usuarioCadastrado = await resposta.json();
+                console.log('Usuário cadastrado:', usuarioCadastrado);
+            } catch (error) {
+                console.error('Erro ao processar resposta da API. Erro ao analisar JSON:', error);
+            }
+        } else {
+            console.error('Erro ao cadastrar:', resposta.statusText);
+        }
+    } catch (error) {
+        console.error('Erro ao processar requisição:', error);
+    }
+}
